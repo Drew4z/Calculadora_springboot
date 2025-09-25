5cdcd29695481b1fec81d86a9c63fe3d289fe9ac
Drew4z <andrewwac27@gmail.com>
2025-09-25 14:25:50 +0200

    begin to create config,views

---
+++ src/main/java/com/daw/calculadora/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.daw.calculadora.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewController(ViewControllerRegistry registry){
+        registry.addViewController("/login").setViewName("/login");
+
+    }
+
+}
